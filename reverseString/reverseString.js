48e344afdf8c09b6e445ec0d11f94d984273ddfb
KSheehan <ksheehan927@gmail.com>
2017-11-20 22:08:40 -0500

    reverseString solution

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,16 @@
 var reverseString = function(words) {
-
+  // vars
+  let letters = words.split('');
+  let reversed = [];
+    
+  // loop
+  for (i = 0; i < letters.length; i++) {
+      // unshift letter into reversed
+      reversed.unshift(letters[i]);
+      
+  }
+    return reversed.join('');
+    
 }
 
 module.exports = reverseString
